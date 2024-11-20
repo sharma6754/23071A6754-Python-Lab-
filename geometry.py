c01172f5c939d616b86e82919aa97b6619cc9a68
sharma6754 <151129641+sharma6754@users.noreply.github.com>
2024-11-20 19:32:36 +0530

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,13 @@
+import math
+def square_area(side):
+    return side * side
+def square_perimeter(side):
+    return 4 * side
+def circle_area(radius):
+    return math.pi * radius**2
+def circle_circumference(radius):
+    return 2 * math.pi * radius
+def triangle_area(base, height):
+    return 0.5 * base * height
+def triangle_perimeter(side1, side2, side3):
+    return side1 + side2 + side3
